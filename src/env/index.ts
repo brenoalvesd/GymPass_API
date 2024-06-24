62f32c655a11a318daa1cba78021c4750d7f3964
Breno Alves <breno7572@gmail.com>
2024-06-24 13:38:58 -0300

    Criando alias de importação no tsconfig.json

--- src/env/index.ts
+++ src/env/index.ts
@@ -11,7 +11,7 @@ const _env = envSchema.safeParse(process.env)
 if (_env.error) {
   console.error('Invalid environment variables', _env.error.format())
 
-  throw new Error('Invalid enviroment variables')
+  throw new Error('Invalid environment variables')
 }
 
 export const env = _env.data
