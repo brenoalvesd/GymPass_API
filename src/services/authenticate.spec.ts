462a70e15b621f7f17fcd85f4da9b09e5d5a25d4
Breno Alves <breno7572@gmail.com>
2024-07-01 15:10:36 -0300

    Aplicando método beforeEach nas instâncias dos testes

--- src/services/authenticate.spec.ts
+++ src/services/authenticate.spec.ts
@@ -1,14 +1,18 @@
-import { expect, describe, it } from 'vitest'
+import { expect, describe, it, beforeEach } from 'vitest'
 import { AuthenticateUseCase } from './authenticate'
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
 import { hash } from 'bcryptjs'
 import { InvalidCredentialsError } from './errors/invalid-credentials-error'
 
-describe('Authenticate Use Case', () => {
-  it('should be able to authenticate', async () => {
-    const usersRepository = new InMemoryUsersRepository()
-    const sut = new AuthenticateUseCase(usersRepository) // sut = system under test
+let usersRepository: InMemoryUsersRepository
+let sut: AuthenticateUseCase // sut = system under test
 
+describe('Authenticate Use Case', () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    sut = new AuthenticateUseCase(usersRepository)
+  })
+  it('should be able to authenticate', async () => {
     await usersRepository.create({
       name: 'John Doe',
       email: 'johndoe@example.com',
@@ -24,9 +28,6 @@ describe('Authenticate Use Case', () => {
   })
 
   it('should not be able to authenticate with wrong e-mail', async () => {
-    const usersRepository = new InMemoryUsersRepository()
-    const sut = new AuthenticateUseCase(usersRepository) // sut = system under test
-
     expect(() =>
       sut.execute({
         email: 'johndoe@example.com',
@@ -36,9 +37,6 @@ describe('Authenticate Use Case', () => {
   })
 
   it('should not be able to authenticate with wrong password', async () => {
-    const usersRepository = new InMemoryUsersRepository()
-    const sut = new AuthenticateUseCase(usersRepository) // sut = system under test
-
     await usersRepository.create({
       name: 'John Doe',
       email: 'johndoe@example.com',
