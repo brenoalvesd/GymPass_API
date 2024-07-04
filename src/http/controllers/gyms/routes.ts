876639f69001beb8d33772801f36ca2122e74256
Breno Alves <breno7572@gmail.com>
2024-07-04 12:43:01 -0300

    Criando Controllers de Busca de Academias e Busca de Academias Próximas, adicionando-os no arquivo de Rotas

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -2,6 +2,15 @@ import { FastifyInstance } from 'fastify'
 
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
 
+import { search } from './search-gyms-controller'
+import { nearby } from './fetch-nearby-gyms-controller'
+import { create } from './create-gym-controller'
+
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
+
+  app.get('/gyms/search', search)
+  app.get('gyms/nearby', nearby)
+
+  app.post('/gyms', create)
 }
