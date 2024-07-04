fe6e3c0819818dafb1275a7a05a4265cc39e332f
Breno Alves <breno7572@gmail.com>
2024-07-04 16:51:11 -0300

    Criando Testes E2E para as rotas de Check-In

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -9,9 +9,9 @@ import { validate } from './check-ins-controllers/validate-controller'
 export async function checkInsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
 
-  app.get('/check-ins/hisory', history)
+  app.get('/check-ins/history', history)
   app.get('/check-ins/metrics', metrics)
 
   app.post('/gyms/:gymId/check-ins', create)
-  app.post('/check-ins/:checkInId/validate', validate)
+  app.patch('/check-ins/:checkInId/validate', validate)
 }
