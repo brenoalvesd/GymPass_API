62ec5f10dc058c13bfcc5ef4972e3e5197941889
Breno Alves <breno7572@gmail.com>
2024-07-04 18:41:46 -0300

    Implementando Refresh Token

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -5,10 +5,13 @@ import { verifyJWT } from '@/http/middlewares/verify-jwt'
 import { register } from './users-controllers/register-controller'
 import { authenticate } from './users-controllers/authenticate-controller'
 import { profile } from './users-controllers/profile-controller'
+import { refresh } from './users-controllers/refresh-token-controller'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  app.patch('/token/refresh', refresh)
 
   /** Authenticated */
   app.get('/me', { onRequest: [verifyJWT] }, profile)
